b4017697e9556b84f32c5646ec39a2dcc28440c3
Martin Waite <martin@hiveit.co.uk>
2021-03-12 18:00:38 +0000

    Table tool UI updates

--- src/explore-education-statistics-common/src/modules/table-tool/components/SubjectStep.tsx
+++ src/explore-education-statistics-common/src/modules/table-tool/components/SubjectStep.tsx
@@ -1,3 +1,4 @@
+import InsetText from '@common/components/InsetText';
 import SummaryList from '@common/components/SummaryList';
 import SummaryListItem from '@common/components/SummaryListItem';
 import Tabs from '@common/components/Tabs';
@@ -89,10 +90,17 @@ const SubjectStep = ({
               />
             )}
 
-            <p>If you can't find the table you're looking for, then you can </p>
-            <a className="govuk-button" href={`#${subjectTabIds.createTable}`}>
-              Create your own table
-            </a>
+            <InsetText>
+              <p>
+                If you can't find the table you're looking for, then you can{' '}
+              </p>
+              <a
+                className="govuk-button"
+                href={`#${subjectTabIds.createTable}`}
+              >
+                Create your own table
+              </a>
+            </InsetText>
           </TabsSection>
           <TabsSection
             title="Create your own table"
@@ -101,9 +109,17 @@ const SubjectStep = ({
             {form}
           </TabsSection>
         </Tabs>
+        <h2 className="govuk-heading-m govuk-!-margin-top-9">
+          View or edit previous step
+        </h2>
       </>
     ) : (
-      form
+      <>
+        {form}
+        <h2 className="govuk-heading-m govuk-!-margin-top-9">
+          View or edit previous step
+        </h2>
+      </>
     );
   }
 
@@ -112,11 +128,16 @@ const SubjectStep = ({
 
   return (
     <>
-      {heading}
-
-      <SummaryList noBorder>
-        <SummaryListItem term="Subject">{subjectName}</SummaryListItem>
-      </SummaryList>
+      <div className="govuk-grid-row">
+        <div className="govuk-grid-column-one-quarter">{heading}</div>
+        <div className="govuk-grid-column-three-quarters">
+          <Details summary="View details" className="govuk-!-margin-bottom-2">
+            <SummaryList noBorder>
+              <SummaryListItem term="Subject">{subjectName}</SummaryListItem>
+            </SummaryList>
+          </Details>
+        </div>
+      </div>
     </>
   );
 };
