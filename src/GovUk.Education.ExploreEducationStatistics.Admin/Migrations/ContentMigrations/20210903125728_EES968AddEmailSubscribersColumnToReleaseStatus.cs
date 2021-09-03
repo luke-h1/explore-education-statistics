750ceed33c19fe3cf15819760d48dfb698ab4516
Mark Youngman <mark@hiveit.co.uk>
2021-09-03 13:58:29 +0100

    EES-968 Add migration to create EmailSubscribers column on ReleaseStatus table

---
+++ src/GovUk.Education.ExploreEducationStatistics.Admin/Migrations/ContentMigrations/20210903125728_EES968AddEmailSubscribersColumnToReleaseStatus.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GovUk.Education.ExploreEducationStatistics.Admin.Migrations.ContentMigrations
+{
+    public partial class EES968AddEmailSubscribersColumnToReleaseStatus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "EmailSubscribers",
+                table: "ReleaseStatus",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmailSubscribers",
+                table: "ReleaseStatus");
+        }
+    }
+}
