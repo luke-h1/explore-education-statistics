1cac3b0d26b1c1af0bce9e8aed714fa46c7cdc86
Duncan Watson <duncan@hiveit.co.uk>
2022-06-30 09:38:05 +0100

    EES-3369 - stripping out Data Factory functionality for copying Releases from the private to the public Statistics db

--- src/GovUk.Education.ExploreEducationStatistics.Publisher.Model/PublisherQueues.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Publisher.Model/PublisherQueues.cs
@@ -6,7 +6,6 @@
         public const string PublishAllContentQueue = "publish-all-content";
         public const string PublishMethodologyFilesQueue = "publish-methodology-files";
         public const string PublishReleaseContentQueue = "publish-release-content";
-        public const string PublishReleaseDataQueue = "publish-release-data";
         public const string PublishReleaseFilesQueue = "publish-release-files";
         public const string PublishTaxonomyQueue = "publish-taxonomy";
         public const string RetryStageQueue = "retry";
