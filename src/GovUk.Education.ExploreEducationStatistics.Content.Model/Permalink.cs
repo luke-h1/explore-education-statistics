90a0d2396a0a3b937e37ba4ee34fd4e9ef96c669
Ben Outram <ben@hiveit.co.uk>
2022-09-28 16:29:51 +0100

    EES-3665 Create Permalink as database entity

---
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Model/Permalink.cs
@@ -0,0 +1,15 @@
+﻿#nullable enable
+using System;
+
+namespace GovUk.Education.ExploreEducationStatistics.Content.Model;
+
+public class Permalink
+{
+    public Guid Id { get; init; }
+
+    public Guid? ReleaseId { get; init; }
+
+    public Guid SubjectId { get; init; }
+
+    public DateTime Created { get; init; }
+}
