1575179838538946a84be213350250db5552b0e6
Ben Outram <ben@hiveit.co.uk>
2022-10-24 20:33:31 +0100

    EES-3707 Prototype a Find Stats endpoint returning publications using a db query with free-text search, release type filter, sorting and pagination.

--- src/GovUk.Education.ExploreEducationStatistics.Content.Api/Controllers/ThemeController.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Api/Controllers/ThemeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Net.Mime;
 using System.Threading.Tasks;
 using GovUk.Education.ExploreEducationStatistics.Common.Extensions;
+using GovUk.Education.ExploreEducationStatistics.Content.Model;
+using GovUk.Education.ExploreEducationStatistics.Content.Services;
+using GovUk.Education.ExploreEducationStatistics.Content.Services.Interfaces;
 using GovUk.Education.ExploreEducationStatistics.Content.Services.Interfaces.Cache;
 using GovUk.Education.ExploreEducationStatistics.Content.Services.Requests;
 using GovUk.Education.ExploreEducationStatistics.Content.Services.ViewModels;
@@ -16,13 +19,37 @@ namespace GovUk.Education.ExploreEducationStatistics.Content.Api.Controllers
     {
         private readonly IMethodologyCacheService _methodologyCacheService;
         private readonly IThemeCacheService _themeCacheService;
+        private readonly IThemeService _themeService;
 
-        public ThemeController(
-            IMethodologyCacheService methodologyCacheService,
-            IThemeCacheService themeCacheService)
+        public ThemeController(IMethodologyCacheService methodologyCacheService,
+            IThemeCacheService themeCacheService,
+            IThemeService themeService)
         {
             _methodologyCacheService = methodologyCacheService;
             _themeCacheService = themeCacheService;
+            _themeService = themeService;
+        }
+
+        [HttpGet("find-stats-prototype")]
+        public async Task<ActionResult<List<FindStatsPublicationViewModel>>> GetPublications(
+            [FromQuery(Name = "page")] int page,
+            [FromQuery(Name = "pageSize")] int pageSize = 10,
+            [FromQuery(Name = "searchTerm")] string? searchTerm = null,
+            [FromQuery(Name = "releaseType")] ReleaseType? releaseType = null,
+            [FromQuery(Name = "sortBy")] FindStatsSortBy sortBy = FindStatsSortBy.Title,
+            [FromQuery(Name = "sortOrder")] FindStatsSortOrder sortOrder = FindStatsSortOrder.Asc)
+        {
+            // TODO Use Min validation for page and pageSize
+
+            return await _themeService
+                .GetPublications(
+                    releaseType,
+                    searchTerm,
+                    page: page,
+                    pageSize: pageSize,
+                    sortBy,
+                    sortOrder)
+                .HandleFailuresOrOk();
         }
 
         [HttpGet("themes")]
