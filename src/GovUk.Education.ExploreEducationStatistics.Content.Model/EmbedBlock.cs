4a15fed5b518bb51aedcfe51561b54e2b265d83b
Mark Youngman <mark@hiveit.co.uk>
2022-11-14 16:20:01 +0000

    EES-3879 Add new EmbedBlock + EmbedBlockLink ContentBlock type

---
+++ src/GovUk.Education.ExploreEducationStatistics.Content.Model/EmbedBlock.cs
@@ -0,0 +1,15 @@
+﻿#nullable enable
+using System.ComponentModel.DataAnnotations;
+
+namespace GovUk.Education.ExploreEducationStatistics.Content.Model
+{
+    public class EmbedBlock
+    {
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        [Url]
+        public string Url { get; set; }
+    }
+}
