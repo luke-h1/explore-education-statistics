4fd977412232849c47e7ce9c5d7818ed486ae8d4
Ben Outram <ben@hiveit.co.uk>
2023-01-06 18:27:15 +0000

    EES-3945 Add indexed views to assist the SqlServer query optimiser in queries by SubjectId on the Observation table.

---
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Model/Migrations/Current_View_ObservationSubjectIdGeographicLevel.sql
@@ -0,0 +1,18 @@
+﻿-- Create a view using the WITH SCHEMABINDING option
+CREATE VIEW dbo.ObservationSubjectIdGeographicLevel
+    WITH SCHEMABINDING
+    AS
+    SELECT
+        SubjectId,
+        GeographicLevel,
+        COUNT_BIG(*) AS Count
+    FROM dbo.Observation
+    INNER JOIN dbo.Location
+    ON Observation.LocationId = Location.Id
+    GROUP BY SubjectId, GeographicLevel
+GO
+
+-- Create an index on the view
+CREATE UNIQUE CLUSTERED INDEX IX_ObservationSubjectIdGeographicLevel_SubjectId_GeographicLevel
+    ON dbo.ObservationSubjectIdGeographicLevel(SubjectId,GeographicLevel)
+GO
---
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Model/Migrations/Current_View_ObservationSubjectIdYearTimeIdentifier.sql
@@ -0,0 +1,17 @@
+﻿-- Create a view using the WITH SCHEMABINDING option
+CREATE VIEW dbo.ObservationSubjectIdYearTimeIdentifier
+    WITH SCHEMABINDING
+    AS
+    SELECT
+        SubjectId,
+        Year,
+        TimeIdentifier,
+        COUNT_BIG(*) AS Count
+    FROM dbo.Observation
+    GROUP BY SubjectId, Year, TimeIdentifier
+GO
+
+-- Create an index on the view
+CREATE UNIQUE CLUSTERED INDEX IX_ObservationSubjectIdYearTimeIdentifier_SubjectId_Year_TimeIdentifier
+    ON dbo.ObservationSubjectIdYearTimeIdentifier(SubjectId,Year,TimeIdentifier)
+GO
