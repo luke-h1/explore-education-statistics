0fa5412a976b47278e7312471697dd537e49914f
Duncan Watson <duncan@hiveit.co.uk>
2023-01-24 14:22:24 +0000

    EES-3948 - updating `config` value for example value from the Middleware documentation as an example of how to ignore many of the non-application routes when deciding which routes to invoke the middleware behaviour on

--- src/explore-education-statistics-frontend/src/middleware.ts
+++ src/explore-education-statistics-frontend/src/middleware.ts
@@ -32,3 +32,16 @@ export default async function middleware(request: NextRequest) {
 // export const config = {
 //   matcher: '/find-statistics',
 // }
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
+};
